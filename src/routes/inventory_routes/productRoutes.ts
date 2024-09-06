545f2dc5627d8d186b39604b6475926f5fe02273
Santhoshkumarz <id.santhoshk@gmail.com>
2024-09-06 16:02:59 +0530

    Make a Validation

--- src/routes/inventory_routes/productRoutes.ts
+++ src/routes/inventory_routes/productRoutes.ts
@@ -1,18 +1,24 @@
-import express from "express";
+// src/routes/productRoutes.ts
+import express from 'express';
 import {
   getProductById,
   addProduct,
   getProducts,
   updateProduct,
   deleteProduct
-} from "../../controllers/inventory_controllers/productController";
+} from '../../controllers/inventory_controllers/productController';
+import {
+  productValidation,
+  productIdValidation,
+  validate
+} from '../../validation/inventory/productValidation';
 
 const router = express.Router();
 
-router.post("/", addProduct);
+router.post("/", productValidation, validate, addProduct);
 router.get("/", getProducts);
-router.get("/:id", getProductById);
-router.put("/:id", updateProduct);   // Add this line
-router.delete("/:id", deleteProduct); // Add this line
+router.get("/:id", productIdValidation, validate, getProductById);
+router.put("/:id", productIdValidation, productValidation, validate, updateProduct);
+router.delete("/:id", productIdValidation, validate, deleteProduct);
 
 export default router;
