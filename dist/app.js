1bd4e6b0c3f2965a5227a07920b4e08ff090db4a
Santhoshkumarz <id.santhoshk@gmail.com>
2024-09-11 10:50:08 +0530

    build deploy

--- dist/app.js
+++ dist/app.js
@@ -1,9 +1,7 @@
 "use strict";
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
@@ -12,10 +10,10 @@ dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.get("/", (req, res) => {
-  res.send("Welcome to our vrindavan");
+    res.send("Welcome to our vrindavan");
 });
 app.use("/api", routes_1.default);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
