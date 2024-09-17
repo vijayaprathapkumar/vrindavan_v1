06fbb75bc8750098c91012cc37d3ca6980fa9c0b
Santhoshkumarz <id.santhoshk@gmail.com>
2024-09-17 18:45:23 +0530

    Orders build

--- dist/src/controllers/orders/hubOrdersController.js
+++ dist/src/controllers/orders/hubOrdersController.js
@@ -1,16 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getRouteOrders = void 0;
-const routeOrdersModel_1 = require("../../models/orders/routeOrdersModel");
+exports.getHubOrders = void 0;
 const responseHandler_1 = require("../../utils/responseHandler");
-// Get all orders
-const getRouteOrders = async (req, res) => {
+const hubOrdersModel_1 = require("../../models/orders/hubOrdersModel");
+const getHubOrders = async (req, res) => {
     try {
-        const orders = await (0, routeOrdersModel_1.getAllRouteOrders)();
+        const orders = await (0, hubOrdersModel_1.getAllHubOrders)();
         res.status(200).json((0, responseHandler_1.createResponse)(200, 'Orders fetched successfully', orders));
     }
     catch (error) {
         res.status(500).json((0, responseHandler_1.createResponse)(500, 'Error fetching orders', error));
     }
 };
-exports.getRouteOrders = getRouteOrders;
+exports.getHubOrders = getHubOrders;
