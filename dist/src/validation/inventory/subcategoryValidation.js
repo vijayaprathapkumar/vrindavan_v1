4b55ac5a2a85784ae95c7ac8ecb5e0c25e4f0e1b
Santhoshkumarz <id.santhoshk@gmail.com>
2024-09-18 10:48:42 +0530

    Subcategroies rework

--- dist/src/validation/inventory/subcategoryValidation.js
+++ dist/src/validation/inventory/subcategoryValidation.js
@@ -2,41 +2,23 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.validate = exports.subCategoryIdValidation = exports.subCategoryValidation = void 0;
 const express_validator_1 = require("express-validator");
-// Validation rules for subcategory
+const responseHandler_1 = require("../../utils/responseHandler");
+// Validation for subcategory fields
 exports.subCategoryValidation = [
-    (0, express_validator_1.body)('category_id')
-        .isInt({ gt: 0 })
-        .withMessage('Category ID must be a positive integer')
-        .notEmpty()
-        .withMessage('Category ID is required'),
-    (0, express_validator_1.body)('name')
-        .isString()
-        .withMessage('Name must be a string')
-        .notEmpty()
-        .withMessage('Name is required'),
-    (0, express_validator_1.body)('description')
-        .isString()
-        .withMessage('Description must be a string'),
-    (0, express_validator_1.body)('weightage')
-        .isString()
-        .withMessage('Weightage must be a string')
-        .notEmpty()
-        .withMessage('Weightage is required'),
-    (0, express_validator_1.body)('active')
-        .isBoolean()
-        .withMessage('Active status must be a boolean')
+    (0, express_validator_1.body)("category_id").optional().isNumeric().withMessage("Category ID must be a number"),
+    (0, express_validator_1.body)("name").notEmpty().withMessage("Subcategory name is required"),
+    (0, express_validator_1.body)("weightage").notEmpty().withMessage("Weightage is required"),
+    (0, express_validator_1.body)("active").isBoolean().withMessage("Active must be a boolean")
 ];
-// Validation rule for subcategory ID
+// Validation for subcategory ID
 exports.subCategoryIdValidation = [
-    (0, express_validator_1.param)('id')
-        .isInt({ gt: 0 })
-        .withMessage('Subcategory ID must be a positive integer')
+    (0, express_validator_1.param)("id").isNumeric().withMessage("Subcategory ID must be a number")
 ];
-// Middleware to handle validation errors
+// Middleware to validate request
 const validate = (req, res, next) => {
     const errors = (0, express_validator_1.validationResult)(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+        return res.status(400).json((0, responseHandler_1.createResponse)(400, "Validation error", errors.array()));
     }
     next();
 };
