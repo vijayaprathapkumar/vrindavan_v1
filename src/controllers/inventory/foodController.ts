0d1e4068fb550da834dd0d606ce989c02ca6d353
Santhoshkumarz <id.santhoshk@gmail.com>
2024-09-20 18:35:45 +0530

    Filter options

--- src/controllers/inventory/foodController.ts
+++ src/controllers/inventory/foodController.ts
@@ -4,10 +4,16 @@ import { createResponse } from "../../utils/responseHandler";
 
 export const getAllFoods = async (req: Request, res: Response) => {
   try {
-    const foods = await foodModel.getAllFoods();
-    res
-      .status(200)
-      .json(createResponse(200, "Foods fetched successfully", foods));
+    const { status, categoryId, subcategoryId,searchTerm } = req.query;
+    const filters = {
+      status: status ? status === 'true' : undefined,
+      categoryId: categoryId ? Number(categoryId) : undefined,
+      subcategoryId: subcategoryId ? Number(subcategoryId) : undefined,
+      searchTerm: searchTerm ? String(searchTerm) : undefined,
+    };
+    
+    const foods = await foodModel.getAllFoods(filters);
+    res.status(200).json(createResponse(200, "Foods fetched successfully", foods));
   } catch (error) {
     res.status(500).json(createResponse(500, "Error fetching foods", error));
   }
@@ -18,9 +24,7 @@ export const getFoodById = async (req: Request, res: Response) => {
     const { id } = req.params;
     const food = await foodModel.getFoodById(Number(id));
     if (food) {
-      res
-        .status(200)
-        .json(createResponse(200, "Food fetched successfully", food));
+      res.status(200).json(createResponse(200, "Food fetched successfully", food));
     } else {
       res.status(404).json(createResponse(404, "Food not found"));
     }
@@ -33,9 +37,7 @@ export const createFood = async (req: Request, res: Response) => {
   try {
     const foodData = req.body;
     const newFood = await foodModel.createFood(foodData);
-    res
-      .status(201)
-      .json(createResponse(201, "Food created successfully", newFood));
+    res.status(201).json(createResponse(201, "Food created successfully", newFood));
   } catch (error) {
     res.status(500).json(createResponse(500, "Error creating food", error));
   }
@@ -47,9 +49,7 @@ export const updateFood = async (req: Request, res: Response) => {
     const foodData = req.body;
     const updatedFood = await foodModel.updateFood(Number(id), foodData);
     if (updatedFood) {
-      res
-        .status(200)
-        .json(createResponse(200, "Food updated successfully", updatedFood));
+      res.status(200).json(createResponse(200, "Food updated successfully", updatedFood));
     } else {
       res.status(404).json(createResponse(404, "Food not found"));
     }
