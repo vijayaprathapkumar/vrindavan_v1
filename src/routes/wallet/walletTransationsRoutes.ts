2dacf6b3afc1ff1faecd121d4b64838c5816d238
Santhoshkumarz <id.santhoshk@gmail.com>
2024-10-20 11:41:55 +0530

    billing api

--- src/routes/wallet/walletTransationsRoutes.ts
+++ src/routes/wallet/walletTransationsRoutes.ts
@@ -9,9 +9,9 @@ const router = Router();
 router.post("/transaction",verifyDeviceToken, walletRecharges); 
 router.get("/transaction/:userId",verifyDeviceToken, getTransactionsByUserId);  
 router.get("/balance/:userId",verifyDeviceToken,getWalletBalance );
-// router.get("/billing_history/:userId",verifyDeviceToken, fetchBillingHistory);
+router.get("/billing_history/:userId",verifyDeviceToken, fetchOrderBillingHistoryForMobile);
 router.get("/billing_history/:userId",verifyDeviceToken, fetchOrderBillingHistory);
-router.get("/billing_history-api/:userId",verifyDeviceToken, fetchOrderBillingHistoryForMobile);
+// router.get("/billing_history-api/:userId",verifyDeviceToken, fetchOrderBillingHistoryForMobile);
 
 
 export default router;
