a9c25a1a60cb8cef039531c31bdaa223a98d2a19
Santhoshkumarz <id.santhoshk@gmail.com>
2024-10-22 15:21:26 +0530

    subscriptions added

--- src/models/subscriptions/cronjobModel.ts
+++ src/models/subscriptions/cronjobModel.ts
@@ -1,18 +1,19 @@
 import moment from "moment";
-import cron from "node-cron";
 import { db } from "../../config/databaseConnection";
 import { FieldPacket, OkPacket, RowDataPacket } from "mysql2";
 
 async function fetchSubscriptions(batchSize, offset) {
   const [subscriptions]: [RowDataPacket[], FieldPacket[]] = await db
     .promise()
-    .query(`SELECT * FROM user_subscriptions WHERE user_id = ? LIMIT ? OFFSET ?`, [
-      12944, 
-      batchSize,
-      offset,
-    ]);
+    .query(
+      `SELECT * FROM user_subscriptions 
+       WHERE user_id = ? AND active = 1 AND is_pause_subscription = 0 
+       LIMIT ? OFFSET ?`,
+      [12947, batchSize, offset]
+    );
   return subscriptions;
 }
+
 
 // Handle the next day's orders based on user subscriptions
 export async function handleNextDayOrders() {
@@ -25,10 +26,12 @@ export async function handleNextDayOrders() {
     const subscriptions = await fetchSubscriptions(batchSize, offset);
     if (subscriptions.length === 0) break;
 
+    const userOrders = {};
+
     for (const sub of subscriptions) {
+      const userId = sub.user_id;
       let quantityToOrder = 0;
 
-      // Determine quantity to order based on subscription type
       if (sub.subscription_type === "everyday") {
         quantityToOrder = sub.quantity;
       } else if (sub.subscription_type === "alternative_day") {
@@ -51,14 +54,31 @@ export async function handleNextDayOrders() {
       }
 
       if (quantityToOrder > 0) {
-        try {
-          await createOrder(sub); // Pass the subscription object
-        } catch (error) {
-          console.error(
-            `Error creating order for subscription ${sub.id}:`,
-            error
-          );
+        if (!userOrders[userId]) {
+          userOrders[userId] = {
+            cartItems: [],
+            totalPrice: 0,
+          };
         }
+
+        const cartItems = await getsubscriptionByUserId(userId);
+        cartItems.forEach((item) => (item.quantity = quantityToOrder));
+        userOrders[userId].cartItems.push(...cartItems);
+      }
+    }
+
+    for (const userId in userOrders) {
+      const { cartItems } = userOrders[userId];
+      const totalPrice = await createOrder(userId, cartItems);
+      try {
+        await addSubscriptionsPlaceOrder({
+          price: totalPrice,
+          userId,
+          status: "active",
+          method: "wallet",
+        });
+      } catch (error) {
+        console.error(`Error processing payment for user ${userId}:`, error);
       }
     }
 
@@ -67,44 +87,204 @@ export async function handleNextDayOrders() {
 }
 
 // Create an order based on subscription
-export const createOrder = async (subscription: any) => {
-  const userId = subscription.user_id; 
+export const createOrder = async (userId, cartItems) => {
   console.log("Creating order for user:", userId);
-  
+
+  let totalPrice = 0;
+  let orderId;
+
   try {
-    const cartItems = await getsubscriptionByUserId(userId);
-
-    if (!cartItems.length) {
-      throw new Error("No items in subscription for user " + userId);
-    }
-
-    // Calculate total price
-    const totalPrice = cartItems.reduce((total, item) => {
-      const itemPrice = item.foodDiscountPrice || item.foodPrice;
-      return total + itemPrice * item.quantity;
-    }, 0);
-
-    const paymentResult = await addSubscriptionsPlaceOrder({
-      price: totalPrice,
-      userId,
-      status: "active",
-      method: "wallet",
-    });
-
-    if (paymentResult.affectedRows === 0) {
-      throw new Error(`Failed to add place order for user ${userId}`);
-    }
+    orderId = await createOrderEntry(userId);
 
     console.log(
-      `Order successfully created for user ${userId} with payment ID: ${paymentResult.insertId}`
+      `Order successfully created for user ${userId}. Order ID: ${orderId}`
     );
+    for (const item of cartItems) {
+      const finalPrice =
+        item.foodDiscountPrice !== null && item.foodDiscountPrice !== undefined
+          ? item.foodDiscountPrice
+          : item.foodPrice;
+
+      const existingFoodOrderSql = `
+        SELECT * FROM food_orders
+        WHERE order_id = ? AND food_id = ?;
+      `;
+
+      const [existingFoodOrders]: [RowDataPacket[], FieldPacket[]] = await db
+        .promise()
+        .query(existingFoodOrderSql, [orderId, item.food_id]);
+      if (existingFoodOrders.length === 0) {
+        const foodOrderSql = `
+          INSERT INTO food_orders (
+            price,
+            quantity,
+            food_id,
+            order_id,
+            created_at,
+            updated_at
+          ) 
+          VALUES (?, ?, ?, ?, NOW(), NOW());
+        `;
+
+        const foodOrderValues = [
+          finalPrice,
+          item.quantity,
+          item.food_id,
+          orderId,
+        ];
+        await db.promise().query(foodOrderSql, foodOrderValues);
+        totalPrice += finalPrice * item.quantity;
+      } else {
+        console.log(
+          `Food item ${item.food_id} already exists for order ${orderId}. Skipping insertion.`
+        );
+      }
+    }
+
+    const [walletRows]: [RowDataPacket[], any] = await db
+      .promise()
+      .query(`SELECT balance FROM wallet_balances WHERE user_id = ?`, [userId]);
+    const beforeBalance = walletRows[0]?.balance;
+
+    const deductionSuccess = await deductFromWalletBalance(userId, totalPrice);
+    if (!deductionSuccess) {
+      throw new Error("Failed to deduct from wallet balance.");
+    }
+
+    const afterBalance = (beforeBalance - totalPrice).toFixed(2);
+
+    await logWalletTransaction(
+      userId,
+      orderId,
+      beforeBalance,
+      totalPrice,
+      afterBalance,
+      `Rs ${totalPrice} deducted for user ID ${userId}`
+    );
+
+    return totalPrice;
   } catch (error) {
     console.error("Error creating order:", error);
+    throw new Error("Failed to create order.");
   }
 };
 
+const logWalletTransaction = async (
+  userId,
+  orderId,
+  beforeBalance,
+  amount,
+  afterBalance,
+  description
+) => {
+  const walletLogSql = `
+    INSERT INTO wallet_logs (
+      user_id, 
+      order_id, 
+      order_date, 
+      order_item_id, 
+      before_balance, 
+      amount, 
+      after_balance, 
+      wallet_type, 
+      description, 
+      created_at, 
+      updated_at
+    ) 
+    VALUES (?, ?, NOW(), ?, ?, ?, ?, ?, ?, NOW(), NOW());
+  `;
+
+  const walletLogValues = [
+    userId,
+    orderId,
+    null, // Assuming order_item_id is not applicable here
+    beforeBalance,
+    amount,
+    afterBalance,
+    "deduction",
+    description,
+  ];
+
+  try {
+    await db.promise().query(walletLogSql, walletLogValues);
+  } catch (error) {
+    console.error("Error logging wallet transaction:", error);
+  }
+};
+
+const createOrderEntry = async (userId) => {
+  const addressSql = `
+    SELECT da.id AS delivery_address_id, da.*, l.route_id, l.hub_id, l.name AS locality_name
+    FROM delivery_addresses da
+    LEFT JOIN localities l ON da.locality_id = l.id
+    WHERE da.user_id = ?;
+  `;
+
+  const [addressRows]: [RowDataPacket[], any] = await db
+    .promise()
+    .query(addressSql, [userId]);
+
+  if (
+    addressRows.length === 0 ||
+    !addressRows[0].locality_id ||
+    !addressRows[0].hub_id
+  ) {
+    throw new Error(
+      `Missing locality or hub information for user ${userId}. Please add the correct address details.`
+    );
+  }
+
+  const addressData = addressRows[0];
+  const { route_id, hub_id, locality_id, delivery_address_id } = addressData;
+console.log('addressData',addressData);
+
+  const orderSql = `
+    INSERT INTO orders (
+      user_id, 
+      order_type, 
+      order_date, 
+      route_id, 
+      hub_id, 
+      locality_id, 
+      delivery_boy_id, 
+      order_status_id, 
+      tax, 
+      delivery_fee, 
+      delivery_address_id,
+      is_wallet_deduct, 
+      created_at, 
+      updated_at
+    ) 
+    VALUES (?, ?, NOW(), ?, ?, ?, ?, ?, ?, ?, ?, ?, NOW(), NOW());
+  `;
+
+  const orderValues = [
+    userId,
+    2, // Assuming order_type is 2
+    route_id,
+    hub_id,
+    locality_id,
+    null, // delivery_boy_id
+    1, // order_status_id (active)
+    0.0, // tax
+    0.0, // delivery_fee
+    delivery_address_id,
+    1, // is_wallet_deduct
+  ];
+
+  const [orderResult]: [OkPacket, any] = await db
+    .promise()
+    .query(orderSql, orderValues);
+
+  if (orderResult.affectedRows === 0) {
+    throw new Error("Failed to create order.");
+  }
+
+  return orderResult.insertId;
+};
+
 // Get subscription details for a specific user
-const getsubscriptionByUserId = async (userId: number, retries = 3) => {
+const getsubscriptionByUserId = async (userId, retries = 3) => {
   const query = `
        SELECT us.*, f.id AS food_id,
         f.name AS food_name,
@@ -145,7 +325,6 @@ const getsubscriptionByUserId = async (userId: number, retries = 3) => {
     return subscriptionRows;
   } catch (error) {
     console.error(`Error fetching subscriptions for user ${userId}:`, error);
-
     if (retries > 0 && error.code === "ETIMEDOUT") {
       console.log(`Retrying... Attempts left: ${retries}`);
       return getsubscriptionByUserId(userId, retries - 1);
@@ -158,23 +337,14 @@ const getsubscriptionByUserId = async (userId: number, retries = 3) => {
 };
 
 // Add a subscription place order
-export const addSubscriptionsPlaceOrder = async (placeOrderData: {
-  price: number;
-  description?: string;
-  userId: number;
-  status: string;
-  method: string;
-}) => {
-  const { price, description, userId, status, method } = placeOrderData;
-  const defaultDescription =
-    description || `Default place order for user ${userId}`;
+export const addSubscriptionsPlaceOrder = async (placeOrderData) => {
+  const { price, userId, status, method } = placeOrderData;
 
   const paymentSql = `
-    INSERT INTO payments (price, description, user_id, status, method, created_at, updated_at) 
-    VALUES (?, ?, ?, ?, ?, NOW(), NOW());
+    INSERT INTO payments (price, user_id, status, method, created_at, updated_at) 
+    VALUES (?, ?, ?, ?, NOW(), NOW());
   `;
-
-  const paymentValues = [price, defaultDescription, userId, status, method];
+  const paymentValues = [price, userId, status, method];
 
   try {
     const [paymentResult]: [OkPacket, any] = await db
@@ -185,196 +355,15 @@ export const addSubscriptionsPlaceOrder = async (placeOrderData: {
       throw new Error("Payment insertion failed.");
     }
 
-    const walletBalanceSql = `
-      SELECT balance FROM wallet_balances WHERE user_id = ?;
-    `;
-
-    const [walletRows]: [RowDataPacket[], any] = await db
-      .promise()
-      .query(walletBalanceSql, [userId]);
-
-    if (walletRows.length === 0) {
-      throw new Error(`No wallet balance found for user ${userId}.`);
-    }
-
-    const beforeBalance = walletRows[0].balance;
-    const afterBalance = (beforeBalance - price).toFixed(2);
-
-    const deductionSuccess = await deductFromWalletBalance(userId, price);
-    if (!deductionSuccess) {
-      throw new Error("Failed to deduct from wallet balance.");
-    }
-
-    const walletLogSql = `
-      INSERT INTO wallet_logs (
-        user_id, 
-        order_id, 
-        order_date, 
-        order_item_id, 
-        before_balance, 
-        amount, 
-        after_balance, 
-        wallet_type, 
-        description, 
-        created_at, 
-        updated_at
-      ) 
-      VALUES (?, ?, NOW(), ?, ?, ?, ?, ?, ?, NOW(), NOW());
-    `;
-
-    const walletLogValues = [
-      userId,
-      null,
-      paymentResult.insertId,
-      beforeBalance,
-      price,
-      afterBalance,
-      "deduction",
-      `Rs ${price} deducted from Rs ${beforeBalance}`,
-    ];
-
-    const addressSql = `
-      SELECT da.id AS delivery_address_id, da.*, l.route_id, l.hub_id, l.name AS locality_name
-      FROM delivery_addresses da
-      LEFT JOIN localities l ON da.locality_id = l.id
-      WHERE da.user_id = ?;
-    `;
-
-    const [addressRows]: [RowDataPacket[], any] = await db
-      .promise()
-      .query(addressSql, [userId]);
-
-    if (
-      addressRows.length === 0 ||
-      !addressRows[0].locality_id ||
-      !addressRows[0].hub_id
-    ) {
-      throw new Error(
-        `Missing locality or hub information for user ${userId}. Please add the correct address details.`
-      );
-    }
-
-    const addressData = addressRows[0];
-    const { route_id, hub_id, locality_id, delivery_address_id } = addressData;
-
-    const orderSql = `
-      INSERT INTO orders (
-        user_id, 
-        order_type, 
-        order_date, 
-        route_id, 
-        hub_id, 
-        locality_id, 
-        delivery_boy_id, 
-        order_status_id, 
-        tax, 
-        delivery_fee, 
-        payment_id, 
-        delivery_address_id,
-        is_wallet_deduct, 
-        created_at, 
-        updated_at
-      ) 
-      VALUES (?, ?, NOW(), ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, NOW(), NOW());
-    `;
-
-    const orderValues = [
-      userId,
-      2, // Assuming order_type is 1
-      route_id,
-      hub_id,
-      locality_id,
-      null, // delivery_boy_id
-      1, // order_status_id
-      0.0, // tax
-      0.0, // delivery_fee
-      paymentResult.insertId,
-      delivery_address_id,
-      1, // is_wallet_deduct
-    ];
-
-    const [orderResult]: [OkPacket, any] = await db
-      .promise()
-      .query(orderSql, orderValues);
-
-    if (orderResult.affectedRows === 0) {
-      throw new Error("Failed to create order.");
-    }
-
-    const orderId = orderResult.insertId;
-
-    walletLogValues[1] = orderId;
-    await db.promise().query(walletLogSql, walletLogValues);
-
-    const cartItems = await getsubscriptionByUserId(userId);
-
-    // Insert into food_orders
-    for (const item of cartItems) {
- console.log('item',item);
- 
-    
-      const finalPrice =
-        item.foodDiscountPrice !== null && item.foodDiscountPrice !== undefined
-          ? item.foodDiscountPrice
-          : item.foodPrice;
-    
-      const foodOrderSql = `
-        INSERT INTO food_orders (
-          price,
-          quantity,
-          food_id,
-          order_id,
-          created_at,
-          updated_at
-        ) 
-        VALUES (?, ?, ?, ?, NOW(), NOW());
-      `;
-    
-      const foodOrderValues = [finalPrice, item.quantity, item.food_id, orderId];
-    
-      await db.promise().query(foodOrderSql, foodOrderValues);
-    }
-    
-
-    await insertIntoOrderPayment(userId, paymentResult.insertId);
-
     return paymentResult;
   } catch (error) {
-    console.error("SQL Error in addPlaceOrder:", error);
+    console.error("SQL Error in addSubscriptionsPlaceOrder:", error);
     throw new Error("Failed to add place order.");
   }
 };
 
-
-export const insertIntoOrderPayment = async (
-  userId: number,
-  paymentId: number
-) => {
-  const orderSql = `
-    UPDATE orders
-    SET payment_id = ?, updated_at = NOW()
-    WHERE user_id = ? AND order_status_id = 1;
-  `;
-
-  const orderValues = [paymentId, userId];
-
-  try {
-    const [result]: [OkPacket, any] = await db
-      .promise()
-      .query(orderSql, orderValues);
-    if (result.affectedRows === 0) {
-      throw new Error("No active order found for the user to update.");
-    }
-  } catch (error) {
-    console.error("SQL Error in insertIntoOrderPayment:", error);
-    throw new Error("Failed to update order with payment details.");
-  }
-};
-
-export const deductFromWalletBalance = async (
-  userId: number,
-  amount: number
-): Promise<boolean> => {
+// Deduct from wallet balance
+export const deductFromWalletBalance = async (userId, amount) => {
   const sql = `
       UPDATE wallet_balances 
       SET balance = balance - ? 
