939c0993e927794bb422517c32889c919606f448
Santhoshkumarz <id.santhoshk@gmail.com>
2024-11-02 14:08:52 +0530

    subcategories image fix

--- src/models/inventory/subcategoryModel.ts
+++ src/models/inventory/subcategoryModel.ts
@@ -31,13 +31,13 @@ export const getAllSubCategoriesWithCategory = async (
       m.order_column,
       m.created_at AS media_created_at,
       m.updated_at AS media_updated_at,
-      CONCAT('https://vrindavanmilk.com/storage/app/public/', m.id, '/', m.file_name) AS original_url
+      CONCAT('https://vrindavanmilk.com/storage/app/public/', m.id, '/conversions/', REPLACE(m.file_name, '.png', '-icon.jpg')) AS original_url
     FROM 
       sub_categories
     LEFT JOIN 
       categories ON sub_categories.category_id = categories.id
     LEFT JOIN 
-      media m ON sub_categories.category_id = m.model_id AND m.model_type = 'App\\\\Models\\\\Category'
+      media m ON sub_categories.id = m.model_id AND m.model_type = 'App\\\\Models\\\\SubCategory'
     WHERE 
       (sub_categories.name LIKE ? OR 
       categories.name LIKE ? OR 
@@ -109,32 +109,32 @@ export const getSubCategoryById = async (
   const [rows] = await db.promise().query<RowDataPacket[]>(
     `
         SELECT 
-  sub_categories.*, 
-  m.id AS media_id,
-  m.model_type,
-  m.model_id,
-  m.uuid,
-  m.collection_name,
-  m.name AS media_name,
-  m.file_name,
-  m.mime_type,
-  m.disk,
-  m.conversions_disk,
-  m.size,
-  m.manipulations,
-  m.custom_properties,
-  m.generated_conversions,
-  m.responsive_images,
-  m.order_column,
-  m.created_at AS media_created_at,
-  m.updated_at AS media_updated_at,
-          CONCAT('https://vrindavanmilk.com/storage/app/public/', m.id, '/', m.file_name) AS original_url
+          sub_categories.*, 
+          m.id AS media_id,
+          m.model_type,
+          m.model_id,
+          m.uuid,
+          m.collection_name,
+          m.name AS media_name,
+          m.file_name,
+          m.mime_type,
+          m.disk,
+          m.conversions_disk,
+          m.size,
+          m.manipulations,
+          m.custom_properties,
+          m.generated_conversions,
+          m.responsive_images,
+          m.order_column,
+          m.created_at AS media_created_at,
+          m.updated_at AS media_updated_at,
+          CONCAT('https://vrindavanmilk.com/storage/app/public/', m.id, '/conversions/', REPLACE(m.file_name, '.png', '-icon.jpg')) AS original_url
         FROM 
           sub_categories
         LEFT JOIN 
           categories ON sub_categories.category_id = categories.id
         LEFT JOIN 
-          media m ON sub_categories.category_id = m.model_id AND m.model_type = 'App\\\\Models\\\\Category'
+          media m ON sub_categories.id = m.model_id AND m.model_type = 'App\\\\Models\\\\SubCategory'
         WHERE 
           sub_categories.id = ?  
         ORDER BY 
