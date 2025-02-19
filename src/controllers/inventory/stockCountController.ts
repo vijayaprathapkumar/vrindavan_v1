f111ee70b0559b047380c955e9b6e9454d76468c
Santhoshkumarz <id.santhoshk@gmail.com>
2025-02-19 13:46:35 +0530

    stock mutaion fix

--- src/controllers/inventory/stockCountController.ts
+++ src/controllers/inventory/stockCountController.ts
@@ -1,22 +1,24 @@
-
 import { Request, Response } from "express";
-import { getStockSummaryModel } from "../../models/inventory/StockModels";
+import { createResponse } from "../../utils/responseHandler";
+import { getStockCounts } from "../../models/inventory/StockModels";
 
-export const getStockCount = async (req: Request, res: Response) => {
-    try {
-      const stockData = await getStockSummaryModel();
-  
-      return res.status(200).json({
-        statusCode: 200,
-        message: "Stock count fetched successfully",
-        data: stockData,
-      });
-    } catch (error) {
-      console.error("Error fetching stock count:", error);
-      return res.status(500).json({
-        statusCode: 500,
-        message: "Error fetching stock count",
-        error: error.message,
-      });
-    }
-  };
+export const getStockSummary = async (req: Request, res: Response) => {
+  try {
+    const stock = await getStockCounts();
+
+    return res.status(200).json(
+      createResponse(200, "Stock summary fetched successfully", {
+        stock: {
+          lowStockCount: stock.lowStockCount,
+          inStockCount: stock.inStockCount,
+          outOfStockCount: stock.outOfStockCount,
+        },
+      })
+    );
+  } catch (error) {
+    console.error("Error fetching stock summary:", error);
+    return res.status(500).json(
+      createResponse(500, "Error fetching stock summary", error.message)
+    );
+  }
+};
