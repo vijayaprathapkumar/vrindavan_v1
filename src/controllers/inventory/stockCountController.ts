bc65651a96142f83175c30675a26213a4854098d
Santhoshkumarz <id.santhoshk@gmail.com>
2025-04-25 21:32:01 +0530

    fix build stock

--- src/controllers/inventory/stockCountController.ts
+++ src/controllers/inventory/stockCountController.ts
@@ -2,7 +2,6 @@ import { Request, Response } from "express";
 import { createResponse } from "../../utils/responseHandler";
 import {
   getStockCounts,
-  getFilteredProducts,
 } from "../../models/inventory/StockModels";
 
 export const getStockSummary = async (req: Request, res: Response) => {
@@ -29,30 +28,5 @@ export const getStockSummary = async (req: Request, res: Response) => {
     return res
       .status(500)
       .json(createResponse(500, "Error fetching stock summary", error.message));
-  }
-};
-
-export const getFilteredStockProducts = async (req: Request, res: Response) => {
-  try {
-    const { stockType } = req.query;
-    if (!stockType || typeof stockType !== "string") {
-      return res
-        .status(400)
-        .json(createResponse(400, "Stock type is required"));
-    }
-
-    const products = await getFilteredProducts(stockType);
-    return res.status(200).json(
-      createResponse(200, "Filtered products fetched successfully", {
-        products,
-      })
-    );
-  } catch (error) {
-    console.error("Error fetching filtered products:", error);
-    return res
-      .status(500)
-      .json(
-        createResponse(500, "Error fetching filtered products", error.message)
-      );
   }
 };
