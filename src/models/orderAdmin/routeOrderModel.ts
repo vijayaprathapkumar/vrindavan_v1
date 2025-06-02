75f7dd7a212c7b88e7690df56353345ad7abf3f5
Santhoshkumarz <id.santhoshk@gmail.com>
2025-06-02 16:39:26 +0530

    devlivery api

--- src/models/orderAdmin/routeOrderModel.ts
+++ src/models/orderAdmin/routeOrderModel.ts
@@ -43,15 +43,24 @@ export const getAllFoodOrders = async (
     f.name AS product_name,
     f.unit AS unit_size,
     SUM(fo.quantity) AS total_quantity,
-    MAX(o.order_date) AS order_date
-  
+    MAX(o.order_date) AS order_date,
+    f.price AS unit_price,
+    f.discount_price,
+    (
+      SUM(fo.quantity) * 
+      CASE 
+        WHEN f.discount_price IS NOT NULL AND f.discount_price > 0 
+        THEN f.discount_price 
+        ELSE f.price 
+      END
+    ) AS total_amount
   FROM food_orders fo
   JOIN orders o ON fo.order_id = o.id
   JOIN foods f ON fo.food_id = f.id
   JOIN hubs h ON o.hub_id = h.id
   JOIN truck_routes tr ON h.route_id = tr.id
   ${conditions}
-  GROUP BY tr.name, h.name, f.name, f.unit
+  GROUP BY tr.name, h.name, f.name, f.unit,f.price, f.discount_price
   LIMIT ? OFFSET ?
 `;
 
@@ -119,7 +128,17 @@ export const getFoodOrderSummary = async (
       f.name AS product_name,
       f.unit AS unit_size,
       SUM(fo.quantity) AS total_quantity,
-      pt.weightage AS product_type_weightage
+      pt.weightage AS product_type_weightage,
+       f.price AS unit_price,
+    f.discount_price,
+    (
+      SUM(fo.quantity) * 
+      CASE 
+        WHEN f.discount_price IS NOT NULL AND f.discount_price > 0 
+        THEN f.discount_price 
+        ELSE f.price 
+      END
+    ) AS total_amount
     FROM food_orders fo
     JOIN orders o ON fo.order_id = o.id
     JOIN foods f ON fo.food_id = f.id
@@ -127,20 +146,29 @@ export const getFoodOrderSummary = async (
     JOIN truck_routes tr ON h.route_id = tr.id
     JOIN product_types pt ON f.product_type_id = pt.id
     ${conditions}
-    GROUP BY f.id, f.name, f.unit, pt.weightage
+    GROUP BY f.id, f.name, f.unit, pt.weightage, f.price, f.discount_price
     ORDER BY total_quantity DESC
     LIMIT ? OFFSET ?
   `;
 
   const countQuery = `
-    SELECT COUNT(DISTINCT f.id) as total
-    FROM food_orders fo
-    JOIN orders o ON fo.order_id = o.id
-    JOIN foods f ON fo.food_id = f.id
-    JOIN hubs h ON o.hub_id = h.id
-    JOIN truck_routes tr ON h.route_id = tr.id
-    JOIN product_types pt ON f.product_type_id = pt.id
-    ${conditions}
+    SELECT COUNT(*) AS total FROM (
+      SELECT 1
+      FROM food_orders fo
+      JOIN orders o ON fo.order_id = o.id
+      JOIN foods f ON fo.food_id = f.id
+      JOIN hubs h ON o.hub_id = h.id
+      JOIN truck_routes tr ON h.route_id = tr.id
+      LEFT JOIN delivery_boys db ON o.delivery_boy_id = db.user_id
+      LEFT JOIN product_types pt ON f.product_type_id = pt.id
+      ${conditions}
+      GROUP BY 
+        tr.id, tr.name, 
+        h.id, h.name, 
+        f.id, f.name, f.unit, f.product_type_id, f.weightage, f.price, f.discount_price,
+        o.delivery_boy_id, db.name,  o.order_date,
+        pt.name, pt.weightage
+    ) AS grouped
   `;
 
   const [[summaryRows], [[{ total }]]] = await Promise.all([
