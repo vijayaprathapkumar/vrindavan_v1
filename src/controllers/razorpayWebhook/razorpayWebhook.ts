2cc4a7a3343a4851bb7ad7ee5c95a66ef2f93eb6
Santhoshkumarz <id.santhoshk@gmail.com>
2025-07-09 23:38:46 +0530

    fix the webhooks v3

--- src/controllers/razorpayWebhook/razorpayWebhook.ts
+++ src/controllers/razorpayWebhook/razorpayWebhook.ts
@@ -120,7 +120,7 @@ export const razorpayWebhookHandler = async (
         extra_amount: extraAmount,
         transaction_amount: amountInRupees,
         transaction_type: payment.method || "unknown",
-        description: payment.description || `Payment via ${payment.method}`
+        description: `Payment via ${payment.method}`
       }
     } as Request;
 
